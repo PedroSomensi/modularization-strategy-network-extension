9d7b98971b8c9b9da9b27e464a51b0425648c2c4
Pedro Victor Somensi <psomensi.contato@gmail.com>
2024-01-27 14:49:10 -0300

    created targets: Network, Feature and NetworkExtension and renamed target aplication to application

---
+++ NetworkExtension/NetworkExtension.h
@@ -0,0 +1,18 @@
+//
+//  NetworkExtension.h
+//  NetworkExtension
+//
+//  Created by Pedro Somensi on 27/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkExtension.
+FOUNDATION_EXPORT double NetworkExtensionVersionNumber;
+
+//! Project version string for NetworkExtension.
+FOUNDATION_EXPORT const unsigned char NetworkExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkExtension/PublicHeader.h>
+
+
