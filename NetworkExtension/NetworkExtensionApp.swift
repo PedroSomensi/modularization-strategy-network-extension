e0253694200503783151ccf86b4c31def5f14f92
Pedro Victor Somensi <psomensi.contato@gmail.com>
2024-01-27 14:41:22 -0300

    created initial project

---
+++ NetworkExtension/NetworkExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkExtensionApp.swift
+//  NetworkExtension
+//
+//  Created by Pedro Somensi on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
